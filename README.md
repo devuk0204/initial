777a14d133f0c1c64bf456e6e11b4909cf7f68aa
devuk <devuk0204@gmail.com>
2025-04-10 19:27:13 +0900

    kubernetes install & add README.md

---
+++ README.md
@@ -0,0 +1,45 @@
+# Don't execute free5gc.sh
+
+# initial_setting.sh comand option
+MASTER(optional) TRUE|FALSE  default: TRUE
+    - TRUE: Install docker-ce
+    - FALSE: Don't install docker-ce
+
+INIT(optional) TRUE|FALSE  default: TRUE
+    - TRUE : If the node is master node and init kubeadm automatically
+    - FALSE : If the node is master node and init kubeadm manually
+
+IF INIT == TRUE
+    MULTI(optional) TRUE|FALSE  default: FALSE
+        - TRUE: If you want to set up the multi-node cluster
+        - FALSE: If you want to setup the single-node cluster
+
+    CNI(optional) flannel|calico|cilium|weave  default:No CNI
+        - option: CNI you want to deploy
+        - flannel: Deploy flannel
+        - calico: Deploy calico
+        - cilium: Deploy Cilium
+        - weave: Deploy weave
+
+
+## custom alias list
+..='cd ..'
+cl='clear'
+upgrade='sudo apt-get update && sudo apt-get uprade -y'
+k='kubectl'
+kp='kubectl get pod -n'
+kpa='kubectl get pod -A'
+kpo='kubectl get pod -A -o wide'
+wkp='watch -n 1 kubectl get pod -A'
+wkpo='watch -n 1 kubectl get pod -A -o wide'
+c='ctr'
+d='docker'
+i='istioctl'
+
+
+## initial_setting.sh demo
+git clone https://github.com/devuk0204/inital
+./initial_setting.sh
+MASTER=FALSE ./initial_setting.sh
+INIT=FALSE ./initial_setting.sh
+INIT=TRUE MULTI=TRUE ./initial_setting.sh
--- initial_setting.sh
+++ initial_setting.sh
@@ -9,63 +9,191 @@ if [ "$NAME" != "Ubuntu" ]; then
     exit
 fi
 
+if 
+
 cd ~
 
-echo "###################################################
+echo "##########################################
+##########################################
+This script automatically sets up the Kubernetes cluster node.
 ##############################################
-alias setting
+##############################################"
+
+
+echo "##########################################
+##########################################
+               alias setting
 ##############################################
 ##############################################"
 
 echo "#custom alias
 alias ..='cd ..'
 alias k='kubectl'
-alias kp='kubectl get pod -A'
+alias kp='kubectl get pod -n'
+alias kpa='kubectl get pod -A'
+alias kpo='kubectl get pod -A -o wide'
 alias wkp='watch -n 1 kubectl get pod -A'
+alias wkpo='watch -n 1 kubectl get pod -A -o wide'
 alias cl='clear'
-alias upgrade='~/tools/upgrade-ubuntu.sh'
+alias upgrade='sudo apt-get update && sudo apt-get upgrade -y'
 alias c='ctr'
 alias d='docker'
 alias i='istioctl'" >> ~/.bashrc
 
 cd ~
 
-echo "####################################################
-##########################################
-cloning tools repo
-##########################################
-##########################################"
-git clone https://github.com/boanlab/tools
-
-
-echo "####################################################
-##########################################
-apt update & upgrade
-##########################################
-##########################################"
-~/tools/upgrade-ubuntu.sh
 
 echo "##########################################
 ##########################################
-install net-tools & golang
+           apt update & upgrade
 ##########################################
 ##########################################"
+sudo apt-get update 
+sudo apt-get -y upgrade 
+sudo apt-get -y autoremove 
+sudo apt-get autoclean
 
-./tools/install-net-tools.sh
-
-./tools/language/install-golang.sh
 
 echo "##########################################
 ##########################################
-install containerd
+            install net-tools
 ##########################################
 ##########################################"
-
-./tools/containers/install-containerd.sh
-sudo chmod 777 /run/containerd/containerd.sock
+sudo apt-get -qq -y install net-tools iproute2 iputils-ping
 
 
-source ~/.bashrc
+echo "##########################################
+##########################################
+        install wget & curl & make
+##########################################
+##########################################"
+sudo apt-get -qq -y install wget curl make
+
+
+echo "##########################################
+##########################################
+             install golang
+##########################################
+##########################################"
+goBinary=$(curl -s https://go.dev/dl/ | grep linux | head -n 1 | cut -d'"' -f4 | cut -d"/" -f3)
+wget https://dl.google.com/go/$goBinary -O /tmp/$goBinary
+sudo tar -C /usr/local -xvzf /tmp/$goBinary
+rm /tmp/$goBinary
+
+echo >> /home/$USER/.bashrc
+echo "export GOPATH=\$HOME/go" >> /home/$USER/.bashrc
+echo "export GOROOT=/usr/local/go" >> /home/$USER/.bashrc
+echo "export PATH=\$PATH:/usr/local/go/bin:\$HOME/go/bin" >> /home/$USER/.bashrc
+echo >> /home/$USER/.bashrc
+mkdir -p /home/$USER/go
+chown -R $USER:$USER /home/$USER/go
+
+
+echo "##########################################
+##########################################
+           install containerd
+##########################################
+##########################################"
+sudo apt-get --qq -y install ca-certificates gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get -qq update
+
+sudo apt-get -qq -y install containerd.io
+
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -i "s/SystemdCgroup = false/SystemdCgroup = true/g" /etc/containerd/config.toml
+sudo systemctl restart containerd
+
+
+echo "##########################################
+##########################################
+          install kubernetes
+##########################################
+##########################################"
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get -qq update
+
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+sudo modprobe br_netfilter
+if [ $(cat /proc/sys/net/bridge/bridge-nf-call-iptables) == 0 ]; then
+    sudo bash -c "echo '1' > /proc/sys/net/bridge/bridge-nf-call-iptables"
+    sudo bash -c "echo 'net.bridge.bridge-nf-call-iptables=1' >> /etc/sysctl.conf"
+fi
 
 
 
+if [ -z "$MASTER" ]; then
+    echo "##########################################
+    ##########################################
+                install docker
+    ##########################################
+    ##########################################"
+    sudo apt-get -qq -y install docker-ce
+    sudo usermod -aG docker $USER
+    sudo chmod 666 /var/run/docker.sock
+elif [ "$MASTER" =="TRUE" ]; then
+    echo "##########################################
+    ##########################################
+                install docker
+    ##########################################
+    ##########################################"
+    sudo apt-get -qq -y install docker-ce
+    sudo usermod -aG docker $USER
+    sudo chmod 666 /var/run/docker.sock
+else
+    exit
+fi
+
+if [ -z "$INIT" ] || [ "$INIT" == "TRUE" ]; then
+echo "##########################################
+##########################################
+              init kubeadm
+##########################################
+##########################################"
+    sudo kubeadm init --pod-network-cidr=10.244.0.0/16 | tee -a ~/k8s_init.log
+
+    if [ -z "$MULTI" ] || [ "$MULTI" == "FALSE" ]; then
+        kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+    fi
+
+    mkdir -p $HOME/.kube
+    sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+    sudo chown $USER:$USER $HOME/.kube/config
+    export KUBECONFIG=$HOME/.kube/config
+    echo "export KUBECONFIG=$HOME/.kube/config" | tee -a ~/.bashrc
+
+    if [ -z "$CNI" ]; then
+        echo "Deploy CNI manually"
+        exit
+    elif [ "$CNI" == "flannel" ]; then
+        kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
+    elif [ "$CNI" == "calico" ]; then
+        kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.4/manifests/calico.yaml
+    elif [ "$CNI" == "cilium" ]; then
+        curl -LO https://github.com/cilium/cilium-cli/releases/latest/download/cilium-linux-amd64.tar.gz
+        sudo tar xzvfC cilium-linux-amd64.tar.gz /usr/local/bin
+        rm cilium-linux-amd64.tar.gz
+        /usr/local/bin/cilium install
+    elif [ "$CNI" == "weave" ]; then
+        kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+    fi
+
+    cat ~/.kube/config
+fi
+
+cat ~/k8s_init.log
+echo "Use the kubeadm join in worker node to add node to the cluster"
+echo "You must execute source ~/.bashrc"
