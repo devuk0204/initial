f18f237414d9e4f6f4d62c4e7d1f281160c5b3bb
devuk <devuk0204@gmail.com>
2025-04-15 14:45:09 +0900

    free5gc2

--- free5gc2.sh
+++ free5gc2.sh
@@ -10,3 +10,19 @@ fi
 sudo apt-get -qq -y install docker-ce
 sudo usermod -aG docker $USER
 sudo chmod 666 /var/run/docker.sock
+
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+bash ./get_helm.sh
+rm ./get_helm.sh
+
+helm version
+
+cd ~
+mkdir 5g
+cd 5g
+helm repo add towards5gs 'https://raw.githubusercontent.com/Orange-OpenSource/towards5gs-helm/main/repo/'
+helm repo update
+helm search repo
+helm pull towards5gs/free5gc; helm pull towards5gs/ueransim
+
+
